96b6d3421ec658d17abb63521dd55c7ca8ec8b8b
deisy johanna basto contreras <deisyjohannabastocontreras@MacBook-Air-de-deisy.local>
2020-11-15 21:46:23 -0500

    'add/my first code/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LenguagesComponent } from './components/shared/lenguages/lenguages.component';
+import { ThemesComponent } from './components/shared/themes/themes.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactmeComponent } from './components/contactme/contactme.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LenguagesComponent,
+    ThemesComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ContactmeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shared/lenguages/lenguages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lenguages',
+  templateUrl: './lenguages.component.html',
+  styleUrls: ['./lenguages.component.scss']
+})
+export class LenguagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
